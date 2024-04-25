456ec9a197e5b11857d41e7cef72342c9b2af807
Brixter Porras <brixterporras@gmail.com>
2024-04-25 23:15:25 +0800

    feat: added initial db connection

--- internal/util/cmdutil.go
+++ internal/util/cmdutil.go
@@ -44,27 +44,32 @@ func NewDatabase(ctx context.Context) (*sql.DB, error) {
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 	}
+	var userCount int
+	if err := db.QueryRow("SELECT COUNT(*) FROM users").Scan(&userCount); err != nil {
+		log.Fatal(err)
+	}
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	stmt, err := tx.Prepare("insert into users(username, password) values(?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(fmt.Sprintf("admin%03d", i+1), "password")
+	if userCount <= 0 {
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		stmt, err := tx.Prepare("insert into users(username, password) values(?, ?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+		for i := 0; i < 100; i++ {
+			_, err = stmt.Exec(fmt.Sprintf("admin%03d", i+1), "password")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		err = tx.Commit()
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	err = tx.Commit()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	rows, err := db.Query("select id, username,password from users")
 	if err != nil {
 		log.Fatal(err)
@@ -82,7 +87,6 @@ func NewDatabase(ctx context.Context) (*sql.DB, error) {
 	}
 	err = db.PingContext(ctx)
 	if err != nil {
-		db.Close() // Close the database if there's an error
 		return nil, err
 	}
 	return db, nil
