1a38711bb8fd12e92f379429aa52562710c4aa64
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2024-05-02 14:01:26 +0800

    Update util.go

--- internal/cmd/util.go
+++ internal/cmd/util.go
@@ -6,11 +6,6 @@ import (
 	"github.com/Brix101/nestfile/internal/settings"
 )
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func generateKey() []byte {
 	k, err := settings.GenerateKey()
